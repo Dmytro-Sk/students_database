ebde7ed27eb590cff1d17af5c743ce1afa37401d
Dmytro-Sk <0971898920d@gmail.com>
2020-09-03 14:03:18 +0300

    Add views, urls and django tags
    - add urls in students/urls.py
    - add views in students/views.py
    - add url tags
    - add block tags

--- students/views.py
+++ students/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def students_list(request):
+    return render(request, 'students/students.html', {})
+
+
+def students_add(request):
+    return render(request, 'students/student_form.html')
+
+
+def students_edit(request, sid):
+    return HttpResponse('<h1>Edit student %s</h1>' % sid)
+
+
+def students_delete(request, sid):
+    return HttpResponse('<h1>Delete student %s</h1>' % sid)
+
+
+def visiting_list(request):
+    return render(request, 'students/visiting.html')
+
+
+def classes_list(request):
+    return render(request, 'students/classes.html')
+
+
+def classes_add(request):
+    return HttpResponse('<h1>Class add form</h1>')
+
+
+def classes_edit(request, gid):
+    return HttpResponse('<h1>Edit class %s</h1>' % gid)
+
+
+def classes_delete(request, gid):
+    return HttpResponse('<h1>delete class %s</h1>' % gid)
+
--- students_database/urls.py
+++ students_database/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from students import views as students_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('students/', students_views.students_list, name='students'),
+    path('students/add', students_views.students_add, name='students-add'),
+    path(r'^students/(?P<sid>\d+)/edit$', students_views.students_edit, name='students-edit'),
+    path(r'^students/(?P<sid>\d+)/delete$', students_views.students_delete, name='students-delete'),
+    path('visiting/', students_views.visiting_list, name='visiting'),
+    path('classes/', students_views.classes_list, name='classes'),
+    path('classes/add', students_views.classes_add, name='classes-add'),
+    path(r'^classes/(?P<gid>\d+)/edit$', students_views.classes_edit, name='classes-edit'),
+    path(r'^classes/(?P<gid>\d+)/delete$', students_views.classes_delete, name='classes-delete'),
 ]
